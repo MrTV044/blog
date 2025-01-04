b58fbd7bf57f9b4c69bd793fcd3aa8aafa008a2b
Arifin Cahyadi Santoso <arifincahyadisantoso@Arifins-MacBook-Pro-2.local>
2025-01-04 20:10:49 +0700

    blog page

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,3 +1,25 @@
+// import Image from "next/image";
+// import { BLOCKS } from "@contentful/rich-text-types";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { BLOCKS } from "@contentful/rich-text-types";
+
+export async function generateStaticParams() {
+  try {
+    const res = await fetch(
+      "https://cdn.contentful.com/spaces/tvndgufzufq7/environments/master/entries?access_token=SzPPef_QzrgCC1tJK4jBJsuR_zLKxxpJcmUTnuUb168&content_type=blogpostPurwa",
+    );
+    const data = await res.json();
+    return data.items.map((el: { fields: { slug: string } }) => {
+      return {
+        slug: el.fields.slug,
+      };
+    });
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
 async function getSinglePost(slug: string) {
   try {
     const res = await fetch(
@@ -18,12 +40,24 @@ export default async function DynamicslugPage({
 }) {
   const slug = (await params).slug;
   const post = await getSinglePost(slug);
+  const data = post.items[0].fields;
 
   console.log(slug);
   console.log(post);
   return (
     <section>
-      <h2>{slug}</h2>
+      <h2>{data.title}</h2>
+      <p>{data.author}</p>
+      {documentToReactComponents(data.content, {
+        renderNode: {
+          [BLOCKS.HEADING_2]: (node, childern) => {
+            return <h2 className="text-xl">{childern}</h2>;
+          },
+          [BLOCKS.PARAGRAPH]: (node, children) => {
+            return <p className="font-semibold text-gray-200">{children}</p>;
+          },
+        },
+      })}
     </section>
   );
 }
